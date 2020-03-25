57d4110db0a25594e97373d6885ee5477d33a685
lotusair <nqh@lotusair.de>
2020-03-26 00:38:33 +0100

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		String what;
+		what="Hello world";
+		
+		what= what + "!";
+		what = what + "?";
+		System.out.println(what + "\nSecondLine");
+	}
+
+}
---
+++ src/PhysicsTeacher.java
@@ -0,0 +1,14 @@
+public class PhysicsTeacher extends Teacher{
+	   String mainSubject = "Physics";
+	   public static void main(String args[]){
+		PhysicsTeacher obj = new PhysicsTeacher();
+		/* Note: we are not accessing the data members 
+		 * directly we are using public getter method 
+		 * to access the private members of parent class
+		 */
+		System.out.println(obj.getCollegeName());
+		System.out.println(obj.getDesignation());
+		System.out.println(obj.mainSubject);
+		obj.does();
+	   }
+}
